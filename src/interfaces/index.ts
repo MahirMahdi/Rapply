0086a89b14bf5470b462ef6a74a4da7c121ce0fe
Mahir <mahdimahir45@gmail.com>
2023-06-30 09:44:18 +0600

    added interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,19 @@
+export interface FeatureCardProps {
+  name: string;
+  description: string;
+  logo: any;
+}
+
+export interface LinkButtonProps {
+  href: string;
+  name: string;
+  placement: string | null;
+  size: string;
+}
+
+export interface OutlinedButtonProps {
+  logo: any;
+  href: string;
+  name: string;
+  placement: string | null;
+}
