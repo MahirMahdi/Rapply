21aaaee29606bfaef767e0ee42a546d95f510820
Mahir <mahdimahir45@gmail.com>
2023-07-04 16:54:07 +0600

    initialized storage

--- src/utility/appwriteClient.ts
+++ src/utility/appwriteClient.ts
@@ -1,4 +1,4 @@
-import { Account, Appwrite, Storage } from "@refinedev/appwrite";
+import { Account, Appwrite, Storage, Databases } from "@refinedev/appwrite";
 
 const APPWRITE_URL = import.meta.env.VITE_APPWRITE_API_ENDPOINT;
 const APPWRITE_PROJECT = import.meta.env.VITE_APPWRITE_PROJECT_ID;
@@ -8,5 +8,6 @@ const appwriteClient = new Appwrite();
 appwriteClient.setEndpoint(APPWRITE_URL).setProject(APPWRITE_PROJECT);
 const account = new Account(appwriteClient);
 const storage = new Storage(appwriteClient);
+const database = new Databases(appwriteClient);
 
-export { account, appwriteClient, storage };
+export { account, appwriteClient, storage, database };
