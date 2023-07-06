b3987380d4458255f3b35caee5668d9ba4cc13aa
Mahir <mahdimahir45@gmail.com>
2023-07-07 00:41:36 +0600

    created sider title logos

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
 import Typography from "@mui/material/Typography";
-import logo from "../../assets/logo.png";
+import logo from "../../assets/logo.webp";
 import { Link } from "react-router-dom";
 import React from "react";
 import { LogoProps } from "../../interfaces";
@@ -38,5 +38,47 @@ const Logo: React.FC<LogoProps> = ({ login, footer }) => {
     </Link>
   );
 };
+
+const TitleLogo = () => {
+  return (
+    <Link to="/" className="router-link">
+      <Box
+        sx={{
+          display: "flex",
+          cursor: "pointer",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src={logo}
+          alt="logo"
+          width={36}
+          height={36}
+          style={{ marginTop: ".25rem" }}
+        />
+        <Typography
+          color="textPrimary"
+          sx={{
+            fontFamily: "'Lexend Variable', sans-serif;",
+            fontWeight: "500",
+            fontSize: "1.75rem",
+          }}
+        >
+          Rapply
+        </Typography>
+      </Box>
+    </Link>
+  );
+};
+
+const CollapsedTitleLogo = () => {
+  return (
+    <Link to="/" className="router-link">
+      <img src={logo} alt="logo" width={40} height={40} />
+    </Link>
+  );
+};
+
+export { TitleLogo, CollapsedTitleLogo };
 
 export default Logo;
