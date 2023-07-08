ca0694e6321a113841d75756677df84fe78fb223
Mahir <mahdimahir45@gmail.com>
2023-07-08 06:31:52 +0600

    created save and close button

--- src/components/buttons/index.tsx
+++ src/components/buttons/index.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import useColorMode from "../../hooks/useColorMode";
 import { OutlinedButtonProps } from "../../interfaces";
+import EditIcon from "@mui/icons-material/Edit";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import CloudUpload from "@mui/icons-material/CloudUpload";
+import ImageIcon from "@mui/icons-material/Image";
+import CloseIcon from "@mui/icons-material/Close";
 
 export const OutlinedButton: React.FC<OutlinedButtonProps> = ({
   logo,
@@ -26,5 +32,92 @@ export const OutlinedButton: React.FC<OutlinedButtonProps> = ({
         {name}
       </button>
     </Link>
+  );
+};
+
+export const EditButton: React.FC<any> = ({ handleClick }) => {
+  const { mode } = useColorMode();
+  return (
+    <Button
+      size="small"
+      sx={{
+        borderRadius: "1.15rem",
+        placeSelf: "flex-end",
+        color: mode === "light" ? "#6505B0" : "inherit",
+        borderColor: mode === "light" ? "#6505B0" : "inherit",
+        padding: { xs: "0", sm: ".25rem .75rem" },
+        mt: { xs: "1.5rem", sm: 0 },
+        "&:hover": {
+          backgroundColor: "#6505B0",
+          borderColor: "#6505B0",
+        },
+        width: "fit-content",
+      }}
+      startIcon={<EditIcon />}
+      variant="outlined"
+      onClick={handleClick}
+    >
+      Edit
+    </Button>
+  );
+};
+
+export const FileUploadButton: React.FC<any> = ({
+  handleFileChange,
+  file,
+  name,
+}) => {
+  return (
+    <label htmlFor="file-input">
+      <input
+        type="file"
+        id="file-input"
+        style={{ display: "none" }}
+        onChange={(e) => handleFileChange(e)}
+      />
+      <Button
+        sx={{ backgroundColor: "#6505B0" }}
+        variant="contained"
+        component="span"
+        startIcon={!file ? <CloudUpload /> : <ImageIcon />}
+      >
+        {file ? `${file.name}` : name}
+      </Button>
+    </label>
+  );
+};
+
+export const SaveButton: React.FC<any> = ({
+  disableCondition,
+  handleClick,
+}) => {
+  return (
+    <Button
+      variant="contained"
+      size="small"
+      sx={{
+        borderRadius: "1.15rem",
+        placeSelf: "flex-end",
+        backgroundColor: "#6505B0",
+        padding: { xs: "0", sm: ".25rem .75rem" },
+        mt: { xs: "1.5rem", sm: 0 },
+        width: "fit-content",
+      }}
+      disabled={disableCondition}
+      onClick={handleClick}
+    >
+      Save
+    </Button>
+  );
+};
+
+export const CloseButton: React.FC<any> = ({ handleClick }) => {
+  return (
+    <IconButton
+      sx={{ position: "absolute", right: "1.25rem", top: ".5rem" }}
+      onClick={() => handleClick()}
+    >
+      <CloseIcon sx={{ fontSize: { xs: "1.15rem", sm: "1.5rem" } }} />
+    </IconButton>
   );
 };
